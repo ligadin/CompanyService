4311d857d3361542ba5d0c2b336e282bf9958e58
João Paulo de Farias <jpaulodefarias@gmail.com>
2020-11-02 22:59:10 -0300

    Bootstrapped project with Spring Initializr

---
+++ src/main/java/br/uece/ligadin/CompanyService/CompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package br.uece.ligadin.CompanyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+
+}
