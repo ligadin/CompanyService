2ec0f9d50678b2d9eb33b9c85dfd8219baf7069e
João Paulo de Farias <jpaulodefarias@gmail.com>
2020-11-18 12:27:32 -0300

    Add Company attributes.

--- src/main/java/br/uece/ligadin/CompanyService/domain/entity/Company.java
+++ src/main/java/br/uece/ligadin/CompanyService/domain/entity/Company.java
@@ -5,7 +5,6 @@ import javax.persistence.*;
 @Entity
 @Table
 public class Company {
-
     @Id
     @GeneratedValue
     private long id;
@@ -16,6 +15,14 @@ public class Company {
     @Column(nullable = false)
     private String website;
 
+    @Column
+    private String email;
+
+    @Column
+    private String size;
+
+    @Column
+    private String industry;
 
     public long getId() {
         return id;
@@ -40,4 +47,28 @@ public class Company {
 	public void setWebsite(String website) {
 		this.website = website;
 	}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
 }
